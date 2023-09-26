41f0c76a9b61e993b559634c87726c5426a1790b
Gabriel Carneiro <gabriel.alvikar@gmail.com>
2023-09-26 03:25:21 -0300

    fix error handle

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,5 @@
 import { Hono } from 'hono'
 
-import HandleError from '@/utils/errorHandle'
-import HandleNotFound from '@/utils/notFoundHandle'
-
 import routes from './routes'
 
 const app = new Hono()
@@ -10,9 +7,6 @@ const app = new Hono()
 const _PORT = 3000
 
 app.route('/', routes)
-
-HandleNotFound.Execute('Error')
-HandleError.Execute()
 
 export default {
   _PORT,
