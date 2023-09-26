2be8c9026f154f8fb18065cd87f658a2623a6a7d
Gabriel Carneiro <gabriel.alvikar@gmail.com>
2023-09-26 06:31:04 -0300

    added error handle

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,19 @@ const _PORT = 3000
 
 app.route('/', routes)
 
+app.notFound((c) => {
+  return c.json({
+    message: 'Oops, page not found. Try again later!'
+  }, 404)
+})
+
+app.onError((err, c) => {
+  console.error(`${err}`)
+  return c.json({
+    message: 'Oops, something went wrong!'
+  }, 500)
+})
+
 export default {
   _PORT,
   fetch: app.fetch
