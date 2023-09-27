990d5c596bb7e9037c344179d37cf9ae58c56e39
Gabriel Carneiro <gabriel.alvikar@gmail.com>
2023-09-27 06:28:45 -0300

    added user controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
 
+import Create from '@/app/controller/userController/create'
+
 const routes = new Hono()
 
 routes.get('/', (c) => c.json({
   message: '😊 Everything is fine!'
 }, 200))
+
+routes.post('/user', Create.Execute)
 
 export default routes
