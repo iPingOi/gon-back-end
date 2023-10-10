edd60b3ec09c24e669c306d6adc452917ce1e1d2
Gabriel Carneiro <gabriel.alvikar@gmail.com>
2023-10-10 18:50:54 -0300

    added images and fix jimp

--- src/app/controllers/jimp/index.ts
+++ src/app/controllers/jimp/index.ts
@@ -2,33 +2,96 @@ import { type Context } from 'hono'
 import Jimp from 'jimp'
 import path from 'path'
 
+import { LATO_BLACK_125_ORANGE, LATO_BLACK_32_GREEN, LATO_BLACK_32_WHITE, LATO_BLACK_35_WHITE } from '@/assets/fonts'
+
 class Create {
   async Jimp (c: Context): Promise<Response> {
-    const image = await Jimp.read(path.join('src', 'images', 'base.png'))
+    const { title, description, price, productImage }: {
+      title: string
+      description: string
+      price: string
+      productImage: string
+    } = await c.req.json()
 
-    const { text } = await c.req.json()
+    const base = await Jimp.read(path.join('src', 'assets', 'images', 'base.png'))
+    const product = await Jimp.read(productImage)
 
-    image.print(
-      await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE),
-      630,
-      880,
+    product.contain(400, 375)
+    base.composite(product, 100, 758)
+
+    base.print(
+      await Jimp.loadFont(LATO_BLACK_35_WHITE),
+      550,
+      758,
       {
-        text,
+        text: title, // 20
         alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
         alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
       },
-      450,
-      157,
-      () => {
-        image.write(path.join('src', 'images', 'edited.png'))
+      550,
+      320
+    )
+
+    base.print(
+      await Jimp.loadFont(LATO_BLACK_32_WHITE),
+      550,
+      1061,
+      {
+        text: description, // 13
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+      },
+      550,
+      69
+    )
+
+    const fontUND = await Jimp.loadFont(LATO_BLACK_32_GREEN)
+
+    base.print(
+      await Jimp.loadFont(LATO_BLACK_125_ORANGE),
+      550,
+      1173,
+      {
+        text: 'R$' + price,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+      },
+      550,
+      220,
+      (_err, base) => {
+        base.print(
+          fontUND,
+          550,
+          1173,
+          {
+            text: 'APENAS',
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_TOP
+          },
+          550,
+          233
+        )
+
+        base.print(
+          fontUND,
+          550,
+          1173,
+          {
+            text: 'A UNIDADE',
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM
+          },
+          550,
+          233
+        )
       }
     )
 
-    // image.write(path.join('src', 'images', 'edited.png'))
+    base.write(path.join('src', 'assets', 'images', 'edited.png'))
 
     return c.json({
-      message: 'ok'
-    })
+      message: 'The image has been created!'
+    }, 201)
   }
 }
 
