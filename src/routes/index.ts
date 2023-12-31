cce5ed9cbc8517f907494cd13f5c1ab378624cf5
Gabriel Carneiro <gabriel.alvikar@gmail.com>
2023-12-31 20:04:40 -0300

    fix: env

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import Middleware from '@/app/middlewares'
 
 const routes = new Hono()
 routes.use('*', cors({
-  origin: process.env.ORIGIN as string
+  origin: process.env.ORIGIN!
 }))
 routes.use('/public/*', serveStatic({ root: './' }))
 
