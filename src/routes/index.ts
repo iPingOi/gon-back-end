084270d3431c3abef8230204c70f0598026f87fd
Gabriel Carneiro de Almeida <gabriel.alvikar@gmail.com>
2024-01-03 14:05:18 -0300

    fix: cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,10 +6,6 @@ import Controller from '@/app/controllers'
 import Middleware from '@/app/middlewares'
 
 const routes = new Hono()
-routes.use('*', cors({
-  origin: 'https://gon-front-end.vercel.app',
-  allowHeaders: ['content-type: application/json', 'accept: application/json']
-}))
 routes.use('/public/*', serveStatic({ root: './' }))
 
 // _GET
@@ -20,9 +16,17 @@ routes.get('/', (c) => c.json({
 routes.get('/product/:id', Controller.ViewProduct.View)
 
 // _POST
-routes.post('/user', Middleware.CreateUser.Create, Controller.CreateUser.Execute)
-routes.post('/create', Controller.CreateJimp.Jimp)
-routes.post('/product', Controller.CreateProduct.Execute)
-routes.post('/auth', Controller.UserAuth.Execute)
+routes.post('/user', cors({
+  origin: 'https://gon-front-end.vercel.app',
+  allowHeaders: ['content-type: application/json', 'accept: application/json']
+}), Middleware.CreateUser.Create, Controller.CreateUser.Execute)
+routes.post('/create', cors({
+  origin: 'https://gon-front-end.vercel.app',
+  allowHeaders: ['content-type: application/json', 'accept: application/json']
+}), Controller.CreateJimp.Jimp)
+routes.post('/product', cors({
+  origin: 'https://gon-front-end.vercel.app',
+  allowHeaders: ['content-type: application/json', 'accept: application/json']
+}), Controller.CreateProduct.Execute)
 
 export default routes
